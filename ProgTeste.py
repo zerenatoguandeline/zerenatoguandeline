2098b6f9bc5509f2ddb37792ed35259816de08bd
Jose Renato Guandeline <zerenatoguandeline@hotmail.com>
2026-01-30 13:03:25 -0300

    ProgTeste.py

--- ProgTeste.py
+++ ProgTeste.py
@@ -15,7 +15,7 @@ st.write(f"Exibindo dados simulados para: **{indicador}**")
 
 # Gerando dados aleatórios para o gráfico
 data = pd.DataFrame(
-    np.random.randn(10, 5),
+    np.random.randn(20, 3),
     columns=['Agro', 'Varejo', 'Transporte']
 )
 
